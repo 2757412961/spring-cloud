40e476e84582f4dfa112971ead90b882ef89b9de
Walk <2757412961@qq.com>
2021-04-11 19:19:03 +0800

    init

---
+++ spring-cloud-api/src/main/java/org/zjh/pojo/Dept.java
@@ -0,0 +1,27 @@
+package org.zjh.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * @author: zjh
+ * @date : 2021/4/9 21:58
+ * @Email : 2757412961@qq.com
+ * @update:
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Dept {
+
+    private Long deptId;
+
+    private String deptName;
+
+    private String dbSource;
+
+}
