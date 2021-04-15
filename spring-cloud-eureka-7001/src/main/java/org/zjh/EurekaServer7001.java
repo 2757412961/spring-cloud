22c8d9403ceabdad5589fe678fb59d6adee6cc24
Walk <2757412961@qq.com>
2021-04-15 10:42:15 +0800

    eureka

---
+++ spring-cloud-eureka-7001/src/main/java/org/zjh/EurekaServer7001.java
@@ -0,0 +1,20 @@
+package org.zjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: zjh
+ * @date : 2021/4/14 21:00
+ * @Email : 2757412961@qq.com
+ * @update:
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7001.class);
+    }
+}
