fb5cf8d45f782e360110be0e29ac3aef7cce4c57
Walk <2757412961@qq.com>
2021-04-17 19:26:38 +0800

    consumer 配置 ribbon

--- spring-cloud-consumer-dept-80/src/main/java/org/zjh/config/ConfigBean.java
+++ spring-cloud-consumer-dept-80/src/main/java/org/zjh/config/ConfigBean.java
@@ -1,5 +1,6 @@
 package org.zjh.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,8 +15,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigBean {
 
+    /**
+     * 配置 Ribbon 负载均衡实现RestTemplate
+     */
     @Bean
-    public RestTemplate getRestTemplate(){
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
 
