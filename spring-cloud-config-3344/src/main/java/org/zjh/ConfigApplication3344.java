fce29339213da92af047624fa81b52ad9f4d41f1
Walk <2757412961@qq.com>
2021-04-21 09:48:03 +0800

    spring cloud config

---
+++ spring-cloud-config-3344/src/main/java/org/zjh/ConfigApplication3344.java
@@ -0,0 +1,20 @@
+package org.zjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: zjh
+ * @date : 2021/4/21 9:06
+ * @Email : 2757412961@qq.com
+ * @update:
+ */
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication3344.class);
+    }
+}
